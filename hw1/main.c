72035e4ba40ad3c8dad833bd035357917687a5a0
madhukararora <madhukar.arora@colorado.edu>
2021-06-05 15:14:41 -0600

    Basic hello world for OpenGL

--- hw1/main.c
+++ hw1/main.c
@@ -1,1 +1,48 @@
+/* CSCI 5229 Computer Graphics
+*  University of Colorado Boulder
+* @author : Madhukar Arora
+* HW1
+*/
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
 
+
+/* function called by GLUT to display the scene */
+void display(void)
+{
+    /* clear screen */
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    /* draw triangle */
+    glBegin(GL_POLYGON);
+    glVertex2f(0.0,0.5);
+    glVertex2f(0.5,0.5);
+    glVertex2f(-0.5,-0.5);
+    glEnd();
+
+    /* make scene visible */
+    glFlush();
+
+}
+
+int main(int argc,char* argv[])
+{
+    /* initialize OpenGL Utility Tool */
+    glutInit(&argc,argv);
+
+    /* Create Window */
+    glutCreateWindow("Hello World");
+
+    /* Register function used to display scene */
+    glutDisplayFunc(display);
+
+    /* pass control for GLUT for events */
+    glutMainLoop();
+
+    /* return to OS */
+    return 0;
+
+}
